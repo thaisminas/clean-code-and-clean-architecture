46a1b258f991274fad0f33a8a806a096b845e320
Thais <thaisminasds@gmail.com>
2023-08-26 19:02:52 -0300

    feat: add route for driver and passenger

--- backend/test/api.test.ts
+++ backend/test/api.test.ts
@@ -33,15 +33,75 @@ test("Deve cadastrar passageiro", async function () {
         email: 'john.doe@gmail.com',
         document: '83432616074'
     };
-    const response1 = await axios.post("http://localhost:3085/passagers", input);
+    const response1 = await axios.post("http://localhost:3085/passengers", input);
     const output1 = response1.data;
     expect(output1.passengerId).toBeDefined()
-
-    const response2 = await axios.get(`http://localhost:3085/passagers/${output1.passengerId}`)
-    const output2 = response2.data;
-    expect(output2.name).toBe('john Doe');
-    expect(output2.email).toBe('john.doe@gmail.com');
-    expect(output2.document).toBe('83432616074')
 });
 
+test("Não deve cadastrar passageiro com cpf inválido", async function () {
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "83432616076",
+        carPlate: "AAA9999"
+    };
+    const response = await axios.post("http://localhost:3085/passengers", input);
+    expect(response.status).toBe(422);
+    const output = response.data;
+    expect(output).toBe("Invalid cpf");
+});
 
+test("Deve obter passageiro", async function () {
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "83432616074"
+    };
+    const response1 = await axios.post("http://localhost:3085/passengers", input);
+    const output1 = response1.data;
+    const response2 = await axios.get(`http://localhost:3085/passengers/${output1.passengerId}`);
+    const output2 = response2.data;
+    expect(output2.passengerData.name).toBe("John Doe");
+    expect(output2.passengerData.email).toBe("john.doe@gmail.com");
+    expect(output2.passengerData.document).toBe("83432616074");
+});
+
+test("Deve cadastrar motorista", async function () {
+    const input = {
+        name: 'john Doe',
+        email: 'john.doe@gmail.com',
+        document: '83432616074',
+        carPlate: 'AAAA999'
+    };
+    const response1 = await axios.post("http://localhost:3085/drivers", input);
+    const output1 = response1.data;
+    expect(output1.driverId).toBeDefined()
+});
+
+test("Não deve cadastrar o motorista", async function () {
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "83432616076",
+        carPlate: "AAA9999"
+    };
+    const response = await axios.post("http://localhost:3085/drivers", input);
+    expect(response.status).toBe(422);
+    const output = response.data;
+    expect(output).toBe("Invalid cpf");
+});
+
+test("Deve obter motorista", async function () {
+    const input = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+        document: "83432616074"
+    };
+    const response1 = await axios.post("http://localhost:3085/drivers", input);
+    const output1 = response1.data;
+    const response2 = await axios.get(`http://localhost:3085/drivers/${output1.driverId}`);
+    const output2 = response2.data;
+    expect(output2.driverData.name).toBe("John Doe");
+    expect(output2.driverData.email).toBe("john.doe@gmail.com");
+    expect(output2.driverData.document).toBe("83432616074");
+});
